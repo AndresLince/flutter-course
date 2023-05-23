2e386e48f8f2a3af228df4cfce12e2b32e9e24bf
AndresLince <lynxdarmatron@gmail.com>
2023-05-23 16:56:45 -0500

    Exercise number 2 dynamic variables

--- 02_variables.dart
+++ 02_variables.dart
@@ -5,9 +5,19 @@ void main() {
     final List<String> abilities = ['impostor'];
     final sprites = <String>['ditto/front.png', 'ditto/back.png'];
 
+    dynamic errorMessage = 'Hola';
+    errorMessage = true;
+    errorMessage = [1, 2, 3, 4, 5, 6];
+    errorMessage = {1, 2, 3, 4, 5, 6};
+    errorMessage = () => true;
+    errorMessage = null;
+
     print("""
     $pokemon
     $hp
     $isAlive
+    $abilities
+    $sprites
+    $errorMessage
     """);
 }
