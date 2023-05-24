2c54baa3287b26f2647c3e3d9c4e3bf9ea58a702
AndresLince <lynxdarmatron@gmail.com>
2023-05-24 06:51:41 -0500

    Exercise number 5 functions WIP

--- 05_functions.dart
+++ 05_functions.dart
@@ -1,7 +1,16 @@
 void main() {
-  print(greetEveryOne());
+  print( greetEveryOne() );
+
+  print( 'Suma: ${ addTwoNumbers(10, 20) }');
+  print( 'Suma: ${ addTwoNumbersOptional(10) }');
 }
 
-String greetEveryOne() {
-  return 'Hello everyone';
+String greetEveryOne() => 'Hello everyone';
+
+int addTwoNumbers(int a,int  b) => a + b;
+
+int addTwoNumbersOptional(int a, [ int  b = 0 ]) {
+  //b ??= 0;
+
+  return a + b;
 }
